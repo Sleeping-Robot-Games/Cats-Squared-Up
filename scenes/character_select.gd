cee6a012a2996b3e8099876d22f9f53cda08a1ff
Bryan <bryan87reed@gmail.com>
2023-03-04 10:55:43 -0700

    Menu and character select

---
+++ scenes/character_select.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var p1_cat
+var p2_cat
+var p1_color: Color = Color(0.83, 0.44, 0.36)
+
+func _ready():
+	for cat in $Cats.get_children():
+		cat.get_node('Button').button_up.connect(_on_cat_select.bind(cat.name.right(1)))
+
+func _on_cat_select(num: String):
+	if p1_cat != null:
+		# Set old color to transparent
+		p1_cat.color.a8 = 0
+	p1_cat = get_node("Cats/Cat" + num)
+	p1_cat.color = p1_color
+	# TODO: put p1_cat details into global state to start the game?
+	
+# TODO: Implement focus change with controller for p2
+
+func _on_fight_button_up():
+	# Check to make sure the player cat info is in global state before continuing
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
---
+++ scripts/menu.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_button_button_up():
+	get_tree().change_scene_to_file("res://scenes/character_select.tscn")
